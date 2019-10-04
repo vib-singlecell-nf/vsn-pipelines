78b872cf237130aad146e55423b24d4c2b168cba
dweemx <maxime.dewaegeneer@kuleuven.vib.be>
2019-10-04 13:06:53 +0200

    Follow PEP8 for bin script in scenic module.

--- src/scenic/bin/grnboost2_without_dask.py
+++ src/scenic/bin/grnboost2_without_dask.py
@@ -19,29 +19,29 @@ from arboreto.core import to_tf_matrix, target_gene_indices, infer_partial_netwo
 parser_grn = argparse.ArgumentParser(description='Run GRNBoost2 using a multiprocessing pool')
 
 parser_grn.add_argument('expression_mtx_fname',
-        type=argparse.FileType('r'),
-        help='The name of the file that contains the expression matrix for the single cell experiment.'
-        ' Two file formats are supported: csv (rows=cells x columns=genes) or loom (rows=genes x columns=cells).')
+                        type=argparse.FileType('r'),
+                        help='The name of the file that contains the expression matrix for the single cell experiment.'
+                        ' Two file formats are supported: csv (rows=cells x columns=genes) or loom (rows=genes x columns=cells).')
 parser_grn.add_argument('tfs_fname',
-        type=argparse.FileType('r'),
-        help='The name of the file that contains the list of transcription factors (TXT; one TF per line).')
+                        type=argparse.FileType('r'),
+                        help='The name of the file that contains the list of transcription factors (TXT; one TF per line).')
 parser_grn.add_argument('-o', '--output',
-        type=argparse.FileType('w'), default=sys.stdout,
-        help='Output file/stream, i.e. a table of TF-target genes (CSV).')
-parser_grn.add_argument('-t', '--transpose', action='store_const', const = 'yes',
-        help='Transpose the expression matrix (rows=genes x columns=cells).')
+                        type=argparse.FileType('w'), default=sys.stdout,
+                        help='Output file/stream, i.e. a table of TF-target genes (CSV).')
+parser_grn.add_argument('-t', '--transpose', action='store_const', const='yes',
+                        help='Transpose the expression matrix (rows=genes x columns=cells).')
 parser_grn.add_argument('--num_workers',
-        type=int, default=cpu_count(),
-        help='The number of workers to use. (default: {}).'.format(cpu_count()))
+                        type=int, default=cpu_count(),
+                        help='The number of workers to use. (default: {}).'.format(cpu_count()))
 parser_grn.add_argument('--seed', type=int, required=False, default=None,
-        help='Seed value for regressor random state initialization (optional)')
+                        help='Seed value for regressor random state initialization (optional)')
 
 parser_grn.add_argument('--cell_id_attribute',
-        type=str, default='CellID',
-        help='The name of the column attribute that specifies the identifiers of the cells in the loom file.')
+                        type=str, default='CellID',
+                        help='The name of the column attribute that specifies the identifiers of the cells in the loom file.')
 parser_grn.add_argument('--gene_attribute',
-        type=str, default='Gene',
-        help='The name of the row attribute that specifies the gene symbols in the loom file.')
+                        type=str, default='Gene',
+                        help='The name of the row attribute that specifies the gene symbols in the loom file.')
 
 args = parser_grn.parse_args()
 
@@ -51,7 +51,7 @@ args = parser_grn.parse_args()
 ################################################################################
 
 
-def runInferPartialNet( target_gene_index ):
+def runInferPartialNet(target_gene_index):
 
     target_gene_name = gene_names[target_gene_index]
     target_gene_expression = expression_matrix[:, target_gene_index]
@@ -66,33 +66,31 @@ def runInferPartialNet( target_gene_index ):
         include_meta=False,
         early_stop_window_length=EARLY_STOP_WINDOW_LENGTH,
         seed=args.seed)
-    return( n )
+    return (n)
 
 if __name__ == '__main__':
 
-    lf = lp.connect( args.expression_mtx_fname.name, mode='r', validate=False )
+    lf = lp.connect(args.expression_mtx_fname.name, mode='r', validate=False)
     # genes in columns:
-    ex_matrix = pd.DataFrame( lf[:,:], index=lf.ra.Gene, columns=lf.ca.CellID ).T
+    ex_matrix = pd.DataFrame(lf[:, :], index=lf.ra.Gene, columns=lf.ca.CellID).T
     lf.close()
     gene_names = ex_matrix.columns
-    print('Loaded expression matrix of {} cells and {} genes...'.format( ex_matrix.shape[0], ex_matrix.shape[1] ) , file=sys.stderr)
+    print('Loaded expression matrix of {} cells and {} genes...'.format(ex_matrix.shape[0], ex_matrix.shape[1]), file=sys.stderr)
 
-    tf_names = load_tf_names( args.tfs_fname.name )
-    print('Loaded {} TFs...'.format( len(tf_names) ) , file=sys.stderr)
+    tf_names = load_tf_names(args.tfs_fname.name)
+    print('Loaded {} TFs...'.format(len(tf_names)), file=sys.stderr)
 
     expression_matrix, gene_names, tf_names = _prepare_input(ex_matrix, gene_names, tf_names)
     tf_matrix, tf_matrix_gene_names = to_tf_matrix(expression_matrix, gene_names, tf_names)
 
-    print('starting GRNBoost2 using {} processes...'.format( args.num_workers ), file=sys.stderr)
+    print('starting GRNBoost2 using {} processes...'.format(args.num_workers), file=sys.stderr)
     start_time = time.time()
 
-    with Pool( args.num_workers ) as p:
-        adjs = p.map(runInferPartialNet, 
-                target_gene_indices(gene_names, target_genes='all')
-                )
-    adj = pd.concat( adjs ).sort_values(by='importance', ascending=False)
+    with Pool(args.num_workers) as p:
+        adjs = p.map(runInferPartialNet,
+                     target_gene_indices(gene_names, target_genes='all'))
+    adj = pd.concat(adjs).sort_values(by='importance', ascending=False)
 
     end_time = time.time()
     print('Done in {} seconds.'.format(end_time - start_time), file=sys.stderr)
-    adj.to_csv( args.output, index=False, sep="\t")
-
+    adj.to_csv(args.output, index=False, sep="\t")
--- src/scenic/bin/merge_SCENIC_motif_track_loom.py
+++ src/scenic/bin/merge_SCENIC_motif_track_loom.py
@@ -8,7 +8,7 @@ import loompy as lp
 from multiprocessing import cpu_count
 from MulticoreTSNE import MulticoreTSNE as TSNE
 import umap
-import json 
+import json
 import zlib
 import base64
 import argparse
@@ -17,12 +17,12 @@ import argparse
 ################################################################################
 
 parser = argparse.ArgumentParser(description='Integrate output from pySCENIC motif- and track-based runs')
-parser.add_argument('--loom_motif', help='Loom file from pySCENIC motif run', required=True, default='pyscenic_motif.loom' )
-parser.add_argument('--loom_track', help='Loom file from pySCENIC track run', required=True, default='pyscenic_motif.loom' )
-parser.add_argument('--loom_output', help='Final loom file with pySCENIC motif and track results integrated', required=True, default='pyscenic.loom' )
+parser.add_argument('--loom_motif', help='Loom file from pySCENIC motif run', required=True, default='pyscenic_motif.loom')
+parser.add_argument('--loom_track', help='Loom file from pySCENIC track run', required=True, default='pyscenic_motif.loom')
+parser.add_argument('--loom_output', help='Final loom file with pySCENIC motif and track results integrated', required=True, default='pyscenic.loom')
 parser.add_argument('--num_workers',
-        type=int, default=cpu_count()-1,
-        help='The number of workers to use. (default: {}).'.format(cpu_count()-1))
+                    type=int, default=(cpu_count() - 1),
+                    help='The number of workers to use. (default: {}).'.format(cpu_count() - 1))
 args = parser.parse_args()
 
 ################################################################################
@@ -32,6 +32,7 @@ args = parser.parse_args()
 # args.loom_track = "/ddn1/vol1/staging/leuven/stg_00002/lcb/cflerin/testruns/SingleCellTxBenchmark/src/singlecelltxbenchmark/pipelines/scenic/work/73/09b0e04f95b803b1a1393e19944df0/auc_trk.loom"
 # args.loom_output = "/ddn1/vol1/staging/leuven/stg_00002/lcb/cflerin/testruns/SingleCellTxBenchmark/src/singlecelltxbenchmark/pipelines/scenic/work/testout.loom"
 
+
 def dfToNamedMatrix(df):
     arr_ip = [tuple(i) for i in df.as_matrix()]
     dtyp = np.dtype(list(zip(df.dtypes.index, df.dtypes)))
@@ -39,81 +40,80 @@ def dfToNamedMatrix(df):
     return arr
 
 
-def integrateMotifTrack( args ):
+def integrateMotifTrack(args):
     ################################################################################
     # load data from loom
     ################################################################################
 
     # scenic motif output
     # scenic output
-    lf = lp.connect( args.loom_motif, mode='r', validate=False )
-    meta_mtf = json.loads(zlib.decompress(base64.b64decode( lf.attrs.MetaData )))
-    auc_mtx_mtf = pd.DataFrame( lf.ca.RegulonsAUC, index=lf.ca.CellID)
-    regulons_mtf = pd.DataFrame( lf.ra.Regulons, index=lf.ra.Gene )
+    lf = lp.connect(args.loom_motif, mode='r', validate=False)
+    meta_mtf = json.loads(zlib.decompress(base64.b64decode(lf.attrs.MetaData)))
+    auc_mtx_mtf = pd.DataFrame(lf.ca.RegulonsAUC, index=lf.ca.CellID)
+    regulons_mtf = pd.DataFrame(lf.ra.Regulons, index=lf.ra.Gene)
     lf.close()
 
-
     # scenic track output
-    lf = lp.connect( args.loom_track, mode='r', validate=False )
-    meta_trk = json.loads(zlib.decompress(base64.b64decode( lf.attrs.MetaData )))
-    auc_mtx_trk = pd.DataFrame( lf.ca.RegulonsAUC, index=lf.ca.CellID)
-    regulons_trk = pd.DataFrame( lf.ra.Regulons, index=lf.ra.Gene )
+    lf = lp.connect(args.loom_track, mode='r', validate=False)
+    meta_trk = json.loads(zlib.decompress(base64.b64decode(lf.attrs.MetaData)))
+    auc_mtx_trk = pd.DataFrame(lf.ca.RegulonsAUC, index=lf.ca.CellID)
+    regulons_trk = pd.DataFrame(lf.ra.Regulons, index=lf.ra.Gene)
     lf.close()
 
     ################################################################################
-    # 
+    #
     ################################################################################
 
     ################################################################################
     # Fix track auc mtx names:
     ################################################################################
 
-    #auc_mtx_mtf.columns = auc_mtx_mtf.columns.str.replace('\(','_(')
-    #auc_mtx_trk.columns = auc_mtx_trk.columns.str.replace('\(','_(')
+    # auc_mtx_mtf.columns = auc_mtx_mtf.columns.str.replace('\(','_(')
+    # auc_mtx_trk.columns = auc_mtx_trk.columns.str.replace('\(','_(')
 
     # relabel columns with suffix indicating the regulon source
-    auc_mtx_trk.columns = auc_mtx_trk.columns+'-track'
-    auc_mtx_mtf.columns = auc_mtx_mtf.columns+'-motif'
+    auc_mtx_trk.columns = auc_mtx_trk.columns + '-track'
+    auc_mtx_mtf.columns = auc_mtx_mtf.columns + '-motif'
 
     # merge the AUC matrices:
-    auc_mtx = pd.concat( [ auc_mtx_mtf, auc_mtx_trk ], sort=False, axis=1, join='outer' )
+    auc_mtx = pd.concat([auc_mtx_mtf, auc_mtx_trk], sort=False, axis=1, join='outer')
 
     # fill NAs (if any) with 0s:
     auc_mtx.fillna(0, inplace=True)
 
     # add underscore for SCope compatibility:
-    auc_mtx.columns = auc_mtx.columns.str.replace('\(','_(')
+    auc_mtx.columns = auc_mtx.columns.str.replace('\(', '_(')
 
     ################################################################################
     # Fix regulon objects to display properly in SCope:
     ################################################################################
 
-    ### combine regulon assignment matrices:
-    regulons_trk.columns = regulons_trk.columns+'-track'
-    regulons_mtf.columns = regulons_mtf.columns+'-motif'
+    # combine regulon assignment matrices:
+    regulons_trk.columns = regulons_trk.columns + '-track'
+    regulons_mtf.columns = regulons_mtf.columns + '-motif'
 
     # merge the regulon assignment matrices:
-    regulons = pd.concat( [ regulons_mtf, regulons_trk ], sort=False, axis=1, join='outer' )
+    regulons = pd.concat([regulons_mtf, regulons_trk], sort=False, axis=1, join='outer')
 
     # replace NAs with 0s:
     regulons.fillna(0, inplace=True)
 
     # add underscore for SCope compatibility:
-    regulons.columns = regulons.columns.str.replace('\(','_(')
+    regulons.columns = regulons.columns.str.replace('\(', '_(')
 
-    ### Rename regulons in the thresholds object, motif
+    # Rename regulons in the thresholds object, motif
     rt_mtf = meta_mtf['regulonThresholds']
-    for i,x in enumerate(rt_mtf):
-        tmp = x.get('regulon').replace("(","_(") +'-motif'
-        x.update( {'regulon': tmp} )
+    for i, x in enumerate(rt_mtf):
+        tmp = x.get('regulon').replace("(", "_(") + '-motif'
+        x.update({'regulon': tmp})
 
-    ### Rename regulons in the thresholds object, track
-    rt_trk= meta_trk['regulonThresholds']
-    for i,x in enumerate(rt_trk):
-        tmp = x.get('regulon').replace("(","_(") +'-track'
-        x.update( {'regulon': tmp} )
+    # Rename regulons in the thresholds object, track
+    rt_trk = meta_trk['regulonThresholds']
+    for i, x in enumerate(rt_trk):
+        tmp = x.get('regulon').replace("(", "_(") + '-track'
+        x.update({'regulon': tmp})
         # blank out the "motifData" field for track-based regulons:
-        x.update( {'mofitData': 'NA.png' }) 
+        x.update({'mofitData': 'NA.png'})
 
     # merge regulon threshold dictionaries:
     rt = rt_mtf + rt_trk
@@ -124,11 +124,11 @@ def integrateMotifTrack( args ):
 
     # UMAP
     runUmap = umap.UMAP(n_neighbors=10, min_dist=0.4, metric='correlation').fit_transform
-    dr_umap = runUmap( auc_mtx.dropna() )
-    #pd.DataFrame(dr_umap, columns=['X', 'Y'], index=auc_mtx.dropna().index).to_csv( "scenic_motif-track_umap.txt", sep='\t')
+    dr_umap = runUmap(auc_mtx.dropna())
+    # pd.DataFrame(dr_umap, columns=['X', 'Y'], index=auc_mtx.dropna().index).to_csv("scenic_motif-track_umap.txt", sep='\t')
     # tSNE
-    tsne = TSNE( n_jobs=args.num_workers )
-    dr_tsne = tsne.fit_transform( auc_mtx.dropna() )
+    tsne = TSNE(n_jobs=args.num_workers)
+    dr_tsne = tsne.fit_transform(auc_mtx.dropna())
 
     ################################################################################
     # embeddings
@@ -136,12 +136,11 @@ def integrateMotifTrack( args ):
 
     defaultEmbedding = pd.DataFrame(dr_umap, columns=['_X', '_Y'], index=auc_mtx.dropna().index)
 
-    Embeddings_X = pd.DataFrame(dr_tsne, columns=['_X', '_Y'], index=auc_mtx.dropna().index).iloc[:,0].astype('float32')
-    Embeddings_Y = pd.DataFrame(dr_tsne, columns=['_X', '_Y'], index=auc_mtx.dropna().index).iloc[:,1].astype('float32')
+    Embeddings_X = pd.DataFrame(dr_tsne, columns=['_X', '_Y'], index=auc_mtx.dropna().index).iloc[:, 0].astype('float32')
+    Embeddings_Y = pd.DataFrame(dr_tsne, columns=['_X', '_Y'], index=auc_mtx.dropna().index).iloc[:, 1].astype('float32')
 
     Embeddings_X.columns = ['1']
     Embeddings_Y.columns = ['1']
-
 
     ################################################################################
     # metadata
@@ -161,13 +160,13 @@ def integrateMotifTrack( args ):
     ]
 
     metaJson["metrics"] = [
-            {
-                "name": "nUMI"
-            }, {
-                "name": "nGene"
-            }, {
-                "name": "Percent_mito"
-            }
+        {
+            "name": "nUMI"
+        }, {
+            "name": "nGene"
+        }, {
+            "name": "Percent_mito"
+        }
     ]
 
     metaJson["annotations"] = [
@@ -181,15 +180,15 @@ def integrateMotifTrack( args ):
     ################################################################################
 
     # re-open the connection to the loom file to copy the original expression data
-    lf = lp.connect( args.loom_motif, mode='r', validate=False )
-    #exprMat = pd.DataFrame( lf[:,:], index=lf.ra.Gene, columns=lf.ca.CellID).T
-    #lf.close()
+    lf = lp.connect(args.loom_motif, mode='r', validate=False)
+    # exprMat = pd.DataFrame(lf[:,:], index=lf.ra.Gene, columns=lf.ca.CellID).T
+    # lf.close()
 
     col_attrs = {
-        "CellID": lf.ca.CellID, #np.array(adata.obs.index),
+        "CellID": lf.ca.CellID,  # np.array(adata.obs.index),
         "Embedding": dfToNamedMatrix(defaultEmbedding),
-        #"Embeddings_X": dfToNamedMatrix(Embeddings_X),
-        #"Embeddings_Y": dfToNamedMatrix(Embeddings_Y),
+        # "Embeddings_X": dfToNamedMatrix(Embeddings_X),
+        # "Embeddings_Y": dfToNamedMatrix(Embeddings_Y),
         "RegulonsAUC": dfToNamedMatrix(auc_mtx),
     }
 
@@ -205,17 +204,14 @@ def integrateMotifTrack( args ):
     attrs['MetaData'] = base64.b64encode(zlib.compress(json.dumps(metaJson).encode('ascii'))).decode('ascii')
 
     lp.create(
-        filename = args.loom_output ,
-        layers=lf[:,:],
-        row_attrs=row_attrs, 
-        col_attrs=col_attrs, 
+        filename=args.loom_output,
+        layers=lf[:, :],
+        row_attrs=row_attrs,
+        col_attrs=col_attrs,
         file_attrs=attrs
     )
     lf.close()
 
 
-
-
 if __name__ == "__main__":
-    integrateMotifTrack( args )
-
+    integrateMotifTrack(args)
