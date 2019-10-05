de750b0c142cea85cecb2acc745a3f899dc0e57f
dweemx <maxime.dewaegeneer@kuleuven.vib.be>
2019-10-05 13:12:02 +0200

    [scenic] Fix bug in range: should start at 0.

--- src/scenic/bin/aggregate_SCENIC_multi_runs_regulons.py
+++ src/scenic/bin/aggregate_SCENIC_multi_runs_regulons.py
@@ -15,10 +15,10 @@ parser_grn = argparse.ArgumentParser(usage="usage: %prog [options] auc_looms",
 parser_grn.add_argument('auc_looms',
                         nargs='+',
                         help='The looms resulting from the SCENIC AUCell step.')
-parser_grn.add_argument('-t', '--regulon-type',
+parser_grn.add_argument('--regulon-type',
                         type=str,
                         dest="regulon_type",
-                        help='The type of the database (either mtf or trk) that was used for motif pruning with cisTarget.')
+                        help='The type of the database ( mtf or trk) used for cisTarget pruning step.')
 parser_grn.add_argument('-n', '--num-runs',
                         type=int,
                         dest="num_runs",
@@ -48,7 +48,7 @@ def from_loom_connection_regulon_incidence_matrix_to_long_df(loom):
 def stack_regulons(auc_looms, regulon_type, num_runs):
     all_runs_regulons_stacked = None
 
-    for i in range(1, len(auc_looms)):
+    for i in range(0, len(auc_looms)):
         loom = lp.connect(filename=auc_looms[i], validate=False)
         df = from_loom_connection_regulon_incidence_matrix_to_long_df(loom=loom)
         if all_runs_regulons_stacked is None:
@@ -65,11 +65,10 @@ def aggregate_genes_by_regulons(all_runs_regulons_stacked):
 
 
 def save_aggregated_regulons(all_runs_regulons_aggregated, regulon_type):
-    os.mkdir(f"regulons_{regulon_type}")
-    print(all_runs_regulons_aggregated.head())
+    os.mkdir(f"multi_runs_regulons_{regulon_type}")
     for regulon_name in np.unique(all_runs_regulons_aggregated["regulon"]):
         regulon_target_gene_occurence_df = all_runs_regulons_aggregated[(all_runs_regulons_aggregated.regulon == regulon_name)].sort_values(by=['count'], ascending=False)[["gene", "count"]]
-        regulon_target_gene_occurence_df.to_csv(path_or_buf=os.path.join(f"regulons_{regulon_type}", regulon_name + ".tsv"), header=False, sep="\t", index=False)
+        regulon_target_gene_occurence_df.to_csv(path_or_buf=os.path.join(f"multi_runs_regulons_{regulon_type}", regulon_name + ".tsv"), header=False, sep="\t", index=False)
 
 all_runs_regulons_stacked = stack_regulons(auc_looms=args.auc_looms, regulon_type=args.regulon_type, num_runs=args.num_runs)
 all_runs_regulons_aggregated = aggregate_genes_by_regulons(all_runs_regulons_stacked=all_runs_regulons_stacked)
