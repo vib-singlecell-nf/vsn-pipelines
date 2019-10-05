8febc5ab1c42868bcf3b6873034bb9fd1f26e864
dweemx <maxime.dewaegeneer@kuleuven.vib.be>
2019-10-05 18:05:22 +0200

    [scenic] Add script and nextflow process to aggregate features (motif or track) from multiple cisTarget runs plus: - Add utility functions (utils.py).

---
+++ bin/aggregate_SCENIC_multi_runs_features.py
@@ -0,0 +1,58 @@
+#!/usr/bin/env python3
+
+import argparse
+import loompy as lp
+import numpy as np
+import pandas as pd
+import os
+from pathlib import Path
+from pyscenic.utils import COLUMN_NAME_MOTIF_SIMILARITY_QVALUE, COLUMN_NAME_ORTHOLOGOUS_IDENTITY, \
+    COLUMN_NAME_MOTIF_ID, COLUMN_NAME_TF, COLUMN_NAME_ANNOTATION, \
+    ACTIVATING_MODULE, REPRESSING_MODULE
+from pyscenic.transform import COLUMN_NAME_NES, COLUMN_NAME_TYPE, COLUMN_NAME_CONTEXT, COLUMN_NAME_TARGET_GENES
+import re
+import sys
+import utils
+
+################################################################################
+################################################################################
+
+
+parser_grn = argparse.ArgumentParser(usage="usage: %prog [options] feature_enrichment_table_fnames",
+                                     description='Aggregate feature (motif or track) enrichment tables from multiple SCENIC runs. ')
+
+parser_grn.add_argument('feature_enrichment_table_fnames',
+                        nargs='+',
+                        help='The looms resulting from the SCENIC AUCell step.')
+parser_grn.add_argument('-o', '--output',
+                        type=argparse.FileType('w'), default=sys.stdout,
+                        help='Output file/stream, i.e. a table of aggregated feature (motif or track) enrichment table (CSV).')
+
+args = parser_grn.parse_args()
+
+
+# Do stuff
+
+def stack_feature_enrichment_tables(feature_enrichment_table_fnames):
+    multi_runs_feature_enrichment_table = None
+
+    def get_type(row):
+        ctx = row[('Enrichment', COLUMN_NAME_CONTEXT)]
+        # Activating is the default!
+        return REPRESSING_MODULE if REPRESSING_MODULE in ctx else ACTIVATING_MODULE
+
+    for i in range(0, len(feature_enrichment_table_fnames)):
+        fname = feature_enrichment_table_fnames[i]
+        feature_enrichment_table = utils.read_feature_enrichment_table(fname=fname, sep=",")
+        # Add the run ID
+        feature_enrichment_table[('', 'RunID')] = int(re.search(r'run_([0-9]+)+__reg_(mtf|trk)\.csv$', str(fname)).group(1))
+        feature_enrichment_table[('', COLUMN_NAME_TYPE)] = feature_enrichment_table.apply(get_type, axis=1)
+        if multi_runs_feature_enrichment_table is None:
+            multi_runs_feature_enrichment_table = feature_enrichment_table
+        else:
+            multi_runs_feature_enrichment_table = pd.concat([multi_runs_feature_enrichment_table, feature_enrichment_table])
+        # print("Reading {0}... Done! ({1})".format(reg_path, multi_runs_feature_enrichment_table.shape))
+    return multi_runs_feature_enrichment_table
+
+multi_runs_feature_enrichment_table = stack_feature_enrichment_tables(args.feature_enrichment_table_fnames)
+multi_runs_feature_enrichment_table.to_csv(path_or_buf=args.output)
