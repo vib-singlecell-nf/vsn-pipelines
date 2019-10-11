ea22c2c31e592afd0f34f82c0efaf83284bdf3ac
Gert Hulselmans <gert.hulselmans@kuleuven.vib.be>
2019-10-11 17:27:38 +0200

    PEP8ify src/scanpy/bin/feature_selection/sc_select_variable_genes.py.
    PEP8ify src/scanpy/bin/feature_selection/sc_select_variable_genes.py.

--- bin/feature_selection/sc_select_variable_genes.py
+++ bin/feature_selection/sc_select_variable_genes.py
@@ -1,40 +1,51 @@
 #!/usr/bin/env python
 import os
 from optparse import OptionParser
+
 import scanpy as sc
 
 parser = OptionParser(usage="usage: %prog [options] h5ad_file_path",
                       version="%prog 1.0")
-parser.add_option("-x", "--method",
-                  type="string",
-                  action="store",
-                  dest="method",
-                  default="mean_disp_plot",
-                  help="Method to choose top variable features. Choose one of : mean_disp_plot")
-parser.add_option("-m", "--min-mean",
-                  type="float",
-                  action="store",
-                  dest="min_mean",
-                  default=None,
-                  help="Select genes with average mean greater than minimum mean.")
-parser.add_option("-M", "--max-mean",
-                  type="float",
-                  action="store",
-                  dest="max_mean",
-                  default=None,
-                  help="Select genes with average mean less than maximum mean.")
-parser.add_option("-d", "--min-dispersion",
-                  type="float",
-                  action="store",
-                  dest="min_disp",
-                  default=None,
-                  help="Select genes with dispersion greater than minimum dispersion.")
-parser.add_option("-D", "--max-dispersion",
-                  type="float",
-                  action="store",
-                  dest="max_disp",
-                  default=None,
-                  help="Select genes with dispersion less than maximum dispersion.")
+parser.add_option(
+    "-x", "--method",
+    type="string",
+    action="store",
+    dest="method",
+    default="mean_disp_plot",
+    help="Method to choose top variable features. Choose one of : mean_disp_plot"
+)
+parser.add_option(
+    "-m", "--min-mean",
+    type="float",
+    action="store",
+    dest="min_mean",
+    default=None,
+    help="Select genes with average mean greater than minimum mean."
+)
+parser.add_option(
+    "-M", "--max-mean",
+    type="float",
+    action="store",
+    dest="max_mean",
+    default=None,
+    help="Select genes with average mean less than maximum mean."
+)
+parser.add_option(
+    "-d", "--min-dispersion",
+    type="float",
+    action="store",
+    dest="min_disp",
+    default=None,
+    help="Select genes with dispersion greater than minimum dispersion."
+)
+parser.add_option(
+    "-D", "--max-dispersion",
+    type="float",
+    action="store",
+    dest="max_disp",
+    default=None,
+    help="Select genes with dispersion less than maximum dispersion."
+)
 (options, args) = parser.parse_args()
 
 # Define the arguments properly
@@ -60,7 +71,8 @@ if options.method == "mean_disp_plot":
         min_mean=options.min_mean,
         max_mean=options.max_mean,
         min_disp=options.min_disp,
-        max_disp=options.max_disp)
+        max_disp=options.max_disp
+    )
     # sc.pl.highly_variable_genes(adata)
 
     # keep only highly variable genes:
