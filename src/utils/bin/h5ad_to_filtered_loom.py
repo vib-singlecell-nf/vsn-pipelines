d4ae4d1f8734c15663909514cc0bbeead21fddda
Gert Hulselmans <gert.hulselmans@kuleuven.vib.be>
2019-10-11 17:44:44 +0200

    PEP8ify src/utils/bin/h5ad_to_filtered_loom.py.
    PEP8ify src/utils/bin/h5ad_to_filtered_loom.py and remove unused imports.
    
    
    Former-commit-id: 24d1716fc30fd97be11319124eccae631289071c

--- src/utils/bin/h5ad_to_filtered_loom.py
+++ src/utils/bin/h5ad_to_filtered_loom.py
@@ -1,16 +1,16 @@
 #!/usr/bin/env python
 import os
 from optparse import OptionParser
-import scanpy as sc
+
 import loompy as lp
-import pandas as pd
-import zlib
-import json
-import base64
 import numpy as np
 
-parser = OptionParser(usage="usage: %prog [options] h5ad_file_path",
-                      version="%prog 1.0")
+import scanpy as sc
+
+parser = OptionParser(
+    usage="usage: %prog [options] h5ad_file_path",
+    version="%prog 1.0"
+)
 (options, args) = parser.parse_args()
 
 # Define the arguments properly
@@ -22,19 +22,18 @@ try:
 except:
     raise Exception("Wrong input format. Expects .h5ad files, got .{}".format(os.path.splitext(FILE_PATH_IN)[0]))
 
-
-row_attrs = { 
-    "Gene": np.array(adata.var.index) ,
+row_attrs = {
+    "Gene": np.array(adata.var.index),
 }
-col_attrs = { 
-    "CellID":  np.array(adata.obs.index) ,
-    "nGene": np.array( np.sum(adata.X.transpose()>0 , axis=0)).flatten() ,
-    "nUMI": np.array( np.sum(adata.X.transpose() , axis=0)).flatten() ,
+col_attrs = {
+    "CellID": np.array(adata.obs.index),
+    "nGene": np.array(np.sum(adata.X.transpose() > 0, axis=0)).flatten(),
+    "nUMI": np.array(np.sum(adata.X.transpose(), axis=0)).flatten(),
 }
 
-lp.create( filename=f"{FILE_PATH_OUT_BASENAME}.loom",
-           layers=(adata.X).T.toarray(), 
-           row_attrs=row_attrs,
-           col_attrs=col_attrs,
+lp.create(
+    filename=f"{FILE_PATH_OUT_BASENAME}.loom",
+    layers=(adata.X).T.toarray(),
+    row_attrs=row_attrs,
+    col_attrs=col_attrs,
 )
-
