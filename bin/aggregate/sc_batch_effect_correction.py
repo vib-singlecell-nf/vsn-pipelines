cac05f19f0838aba07d48be1d992d937248520fb
Gert Hulselmans <gert.hulselmans@kuleuven.vib.be>
2019-10-11 17:24:38 +0200

    PEP8ify src/scanpy/bin/aggregate/sc_batch_effect_correction.py.
    PEP8ify src/scanpy/bin/aggregate/sc_batch_effect_correction.py.

--- bin/aggregate/sc_batch_effect_correction.py
+++ bin/aggregate/sc_batch_effect_correction.py
@@ -1,8 +1,10 @@
 #!/usr/bin/env python
 import os
 from optparse import OptionParser
-import scanpy as sc
+
 import numpy as np
+
+import scanpy as sc
 
 print('''Correct the data from batch effects. Choose one of:
 - bbknn:
@@ -13,73 +15,97 @@ print('''Correct the data from batch effects. Choose one of:
     Details at: see https://icb-scanpy.readthedocs-hosted.com/en/stable/external/scanpy.external.pp.mnn_correct.html
 ''')
 
-parser = OptionParser(usage="usage: %prog [options] h5ad_file_path",
-                      version="%prog 1.0")
-parser.add_option("-x", "--method",
-                  type="string",
-                  action="store",
-                  dest="method",
-                  default="bbknn",
-                  help="Correct the data from batch effects. Choose one of: bkknn, mnn_correct.")
-parser.add_option("-o", "--output-file",
-                  type="string",
-                  action="store",
-                  dest="output_file",
-                  default='',
-                  help="Output file name.")
-parser.add_option("-b", "--batch-key",
-                  type="string",
-                  action="store",
-                  dest="batch_key",
-                  default='batch',
-                  help="[bbknn, mnn_correct], adata.obs column name discriminating between your batches.")
-parser.add_option("-K", "--key",
-                  type="string",
-                  action="store",
-                  dest="key",
-                  default='batch',
-                  help="[combat], adata.obs column name discriminating between your batches.")
-parser.add_option("-c", "--n-pcs",
-                  type="int",
-                  action="store",
-                  dest="n_pcs",
-                  default=50,
-                  help="[bbknn]. How many principal components to use in the analysis.")
-parser.add_option("-k", "--k",
-                  type="int",
-                  action="store",
-                  dest="k",
-                  default=20,
-                  help="[mnn_correct]. Number of mutual nearest neighbors.")
-parser.add_option("-i", "--var-index",
-                  type="string",
-                  action="store",
-                  dest="var_index",
-                  default=None,
-                  help="[mnn_correct]. The index (list of str) of vars (genes). Necessary when using only a subset of vars to perform MNN correction, and should be supplied with var_subset.")
-parser.add_option("-s", "--var-subset",
-                  action="store",
-                  dest="var_subset",
-                  default=None,
-                  help="[mnn_correct]. The subset of vars (list of str) to be used when performing MNN correction. Typically, a list of highly variable genes (HVGs). When set to None, uses all vars.")
-parser.add_option("-j", "--n-jobs",
-                  type="int",
-                  action="store",
-                  dest="n_jobs",
-                  default=None,
-                  help="[bbknn, mnn_correct], The number of jobs. When set to None, automatically uses the number of cores.")
-parser.add_option("-t", "--trim",
-                  type="int",
-                  action="store",
-                  dest="trim",
-                  default=None,
-                  help="[bbknn], Trim the neighbours of each cell to these many top connectivities. May help with population independence and improve the tidiness of clustering.")
-parser.add_option("-n", "--neighbors-within-batch",
-                  type="int",
-                  action="store",
-                  dest="neighbors_within_batch",
-                  default=3,
-                  help="[bbknn], How many top neighbours to report for each batch; total number of neighbours will be this number times the number of batches.")
+parser = OptionParser(
+    usage="usage: %prog [options] h5ad_file_path",
+    version="%prog 1.0"
+)
+parser.add_option(
+    "-x", "--method",
+    type="string",
+    action="store",
+    dest="method",
+    default="bbknn",
+    help="Correct the data from batch effects. Choose one of: bkknn, mnn_correct."
+)
+parser.add_option(
+    "-o", "--output-file",
+    type="string",
+    action="store",
+    dest="output_file",
+    default='',
+    help="Output file name."
+)
+parser.add_option(
+    "-b", "--batch-key",
+    type="string",
+    action="store",
+    dest="batch_key",
+    default='batch',
+    help="[bbknn, mnn_correct], adata.obs column name discriminating between your batches."
+)
+parser.add_option(
+    "-K", "--key",
+    type="string",
+    action="store",
+    dest="key",
+    default='batch',
+    help="[combat], adata.obs column name discriminating between your batches."
+)
+parser.add_option(
+    "-c", "--n-pcs",
+    type="int",
+    action="store",
+    dest="n_pcs",
+    default=50,
+    help="[bbknn]. How many principal components to use in the analysis."
+)
+parser.add_option(
+    "-k", "--k",
+    type="int",
+    action="store",
+    dest="k",
+    default=20,
+    help="[mnn_correct]. Number of mutual nearest neighbors."
+)
+parser.add_option(
+    "-i", "--var-index",
+    type="string",
+    action="store",
+    dest="var_index",
+    default=None,
+    help="[mnn_correct]. The index (list of str) of vars (genes). Necessary when using only a subset of vars to perform MNN correction, and should be supplied with var_subset."
+)
+parser.add_option(
+    "-s", "--var-subset",
+    action="store",
+    dest="var_subset",
+    default=None,
+    help="[mnn_correct]. The subset of vars (list of str) to be used when performing MNN correction. Typically, a list of highly variable genes (HVGs). When set to None, uses all vars."
+)
+parser.add_option(
+    "-j", "--n-jobs",
+    type="int",
+    action="store",
+    dest="n_jobs",
+    default=None,
+    help="[bbknn, mnn_correct], The number of jobs. When set to None, automatically uses the number of cores."
+)
+parser.add_option(
+    "-t", "--trim",
+    type="int",
+    action="store",
+    dest="trim",
+    default=None,
+    help="[bbknn], Trim the neighbours of each cell to these many top connectivities. May help with population independence and improve the tidiness of clustering."
+)
+parser.add_option(
+    "-n", "--neighbors-within-batch",
+    type="int",
+    action="store",
+    dest="neighbors_within_batch",
+    default=3,
+    help="[bbknn], How many top neighbours to report for each batch; total number of neighbours will be this number times the number of batches."
+)
 
 (options, args) = parser.parse_args()
 
@@ -129,6 +155,7 @@ elif options.method == 'mnn':
     # Open issue: https://github.com/chriscainx/mnnpy/issues/24
     #  Use numba==0.43.1
     from mnnpy import mnn_correct
+
     # [mnn_correct] Subset only HVG otherwise "ValueError: Lengths must match to compare"
     corrected = mnn_correct(
         *list(map(lambda adata: adata[:, adata.var.index.isin(hvgs)], adatas)),
