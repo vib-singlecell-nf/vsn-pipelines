2869e3d0d548f6cdc16b4b1923ce9f4614fd0ed2
Gert Hulselmans <gert.hulselmans@kuleuven.vib.be>
2019-10-11 17:46:07 +0200

    PEP8ify src/utils/bin/h5ad_to_loom.py.
    PEP8ify src/utils/bin/h5ad_to_loom.py and remove unused imports.
    
    
    Former-commit-id: 783be3ac22e78225cb2277b8912d442ae1e49da7

--- src/utils/bin/h5ad_to_loom.py
+++ src/utils/bin/h5ad_to_loom.py
@@ -1,26 +1,35 @@
 #!/usr/bin/env python
-import os
-from optparse import OptionParser
-import scanpy as sc
-import loompy as lp
-import pandas as pd
-import zlib
-import json
 import base64
-import numpy as np
+import json
+import os
+import zlib
+from optparse import OptionParser
 
-parser = OptionParser(usage="usage: %prog [options] h5ad_file_path",
-                      version="%prog 1.0")
-parser.add_option("-x", "--method",
-                  action="store",
-                  dest="method",
-                  default="linear_regression",
-                  help="Normalize the data. Choose one of : regress_out")
-parser.add_option("-r", "--variable-to-regress-out",
-                  action="append",
-                  dest="vars_to_regress_out",
-                  default=None,
-                  help="Variable to regress out. To regress multiple variables, add that many -v arguments. Used when running 'regress_out")
+import loompy as lp
+import numpy as np
+import pandas as pd
+
+import scanpy as sc
+
+parser = OptionParser(
+    usage="usage: %prog [options] h5ad_file_path",
+    version="%prog 1.0"
+)
+parser.add_option(
+    "-x", "--method",
+    action="store",
+    dest="method",
+    default="linear_regression",
+    help="Normalize the data. Choose one of : regress_out"
+)
+parser.add_option(
+    "-r", "--variable-to-regress-out",
+    action="append",
+    dest="vars_to_regress_out",
+    default=None,
+    help="Variable to regress out. To regress multiple variables, add that many -v arguments."
+         " Used when running 'regress_out'."
+)
 (options, args) = parser.parse_args()
 
 # Define the arguments properly
@@ -34,16 +43,26 @@ def dfToNamedMatrix(df):
     arr = np.array(arr_ip, dtype=dtyp)
     return arr
 
+
 try:
     adata = sc.read_h5ad(filename=FILE_PATH_IN)
 except:
     raise Exception("Wrong input format. Expects .h5ad files, got .{}".format(os.path.splitext(FILE_PATH_IN)[0]))
 
-ClusterMarkers_0 = pd.DataFrame(index=adata.raw.var.index, columns=[str(x) for x in range(max(set([int(x) for x in adata.obs['louvain']])) + 1)])
+ClusterMarkers_0 = pd.DataFrame(
+    index=adata.raw.var.index,
+    columns=[str(x) for x in range(max(set([int(x) for x in adata.obs['louvain']])) + 1)]
+)
 
-ClusterMarkers_0_avg_logFC = pd.DataFrame(index=adata.raw.var.index, columns=[str(x) for x in range(max(set([int(x) for x in adata.obs['louvain']])) + 1)])
+ClusterMarkers_0_avg_logFC = pd.DataFrame(
+    index=adata.raw.var.index,
+    columns=[str(x) for x in range(max(set([int(x) for x in adata.obs['louvain']])) + 1)]
+)
 
-ClusterMarkers_0_pval = pd.DataFrame(index=adata.raw.var.index, columns=[str(x) for x in range(max(set([int(x) for x in adata.obs['louvain']])) + 1)])
+ClusterMarkers_0_pval = pd.DataFrame(
+    index=adata.raw.var.index,
+    columns=[str(x) for x in range(max(set([int(x) for x in adata.obs['louvain']])) + 1)]
+)
 
 ClusterMarkers_0.fillna(0, inplace=True)
 ClusterMarkers_0_avg_logFC.fillna(0, inplace=True)
@@ -53,12 +72,16 @@ for i in range(max(set([int(x) for x in adata.obs['louvain']])) + 1):
     i = str(i)
     tot_genes = len(adata.uns['rank_genes_groups']['pvals_adj'][i])
     sigGenes = adata.uns['rank_genes_groups']['pvals_adj'][i] < 0.05
-    deGenes = np.logical_and(np.logical_or(adata.uns['rank_genes_groups']['logfoldchanges'][i] >= 1.5, adata.uns['rank_genes_groups']['logfoldchanges'][i] <= -1.5), np.isfinite(adata.uns['rank_genes_groups']['logfoldchanges'][i]))
+    deGenes = np.logical_and(np.logical_or(adata.uns['rank_genes_groups']['logfoldchanges'][i] >= 1.5,
+                                           adata.uns['rank_genes_groups']['logfoldchanges'][i] <= -1.5),
+                             np.isfinite(adata.uns['rank_genes_groups']['logfoldchanges'][i]))
     sigAndDE = np.logical_and(sigGenes, deGenes)
     names = adata.uns['rank_genes_groups']['names'][i][sigAndDE]
     ClusterMarkers_0.loc[names, i] = 1
-    ClusterMarkers_0_avg_logFC.loc[names, i] = np.around(adata.uns['rank_genes_groups']['logfoldchanges'][i][sigAndDE], decimals=6)
-    ClusterMarkers_0_pval.loc[names, i] = np.around(adata.uns['rank_genes_groups']['pvals_adj'][i][sigAndDE], decimals=6)
+    ClusterMarkers_0_avg_logFC.loc[names, i] = np.around(adata.uns['rank_genes_groups']['logfoldchanges'][i][sigAndDE],
+                                                         decimals=6)
+    ClusterMarkers_0_pval.loc[names, i] = np.around(adata.uns['rank_genes_groups']['pvals_adj'][i][sigAndDE],
+                                                    decimals=6)
 
 metaJson = {}
 metaJson["metrics"] = []
@@ -70,7 +93,8 @@ metaJson['embeddings'] = [
     {
         "id": -1,
         "name": f"HVG UMAP"
-    }]
+    }
+]
 
 Embeddings_X = pd.DataFrame()
 Embeddings_Y = pd.DataFrame()
@@ -97,22 +121,25 @@ metaJson['embeddings'].append(
     }
 )
 
-metaJson["clusterings"] = [{
-            "id": 0,
-            "group": "Louvain",
-            "name": "Louvain default resolution",
-            "clusters": [],
-            "clusterMarkerMetrics": [{
-                "accessor": "avg_logFC", 
+metaJson["clusterings"] = [
+    {
+        "id": 0,
+        "group": "Louvain",
+        "name": "Louvain default resolution",
+        "clusters": [],
+        "clusterMarkerMetrics": [
+            {
+                "accessor": "avg_logFC",
                 "name": "Avg. logFC",
                 "description": "Average log fold change from Wilcox test"
             }, {
-                "accessor": "pval", 
+                "accessor": "pval",
                 "name": "Adjusted P-Value",
                 "description": "Adjusted P-Value from Wilcox test"
             }
-            ]                
-        }]
+        ]
+    }
+]
 
 for i in range(max(set([int(x) for x in adata.obs['louvain']])) + 1):
     clustDict = {}
@@ -124,13 +151,14 @@ clusterings = pd.DataFrame()
 
 clusterings["0"] = adata.obs['louvain'].values.astype(np.int64)
 
-col_attrs = {"CellID": np.array(adata.obs.index),
-             "Embedding": dfToNamedMatrix(main_dr),
-             "Embeddings_X": dfToNamedMatrix(Embeddings_X),
-             "Embeddings_Y": dfToNamedMatrix(Embeddings_Y),
-             "Clusterings": dfToNamedMatrix(clusterings),
-             "ClusterID": np.array(adata.obs['louvain'].values)}
-
+col_attrs = {
+    "CellID": np.array(adata.obs.index),
+    "Embedding": dfToNamedMatrix(main_dr),
+    "Embeddings_X": dfToNamedMatrix(Embeddings_X),
+    "Embeddings_Y": dfToNamedMatrix(Embeddings_Y),
+    "Clusterings": dfToNamedMatrix(clusterings),
+    "ClusterID": np.array(adata.obs['louvain'].values)
+}
 
 for col in adata.obs.keys():
     if type(adata.obs[col].dtype) == pd.core.dtypes.dtypes.CategoricalDtype:
@@ -148,14 +176,21 @@ for col in adata.obs.keys():
         )
     col_attrs[col] = np.array(adata.obs[col].values)
 
-
-row_attrs = {"Gene": np.array(adata.raw.var.index),
-             "ClusterMarkers_0": dfToNamedMatrix(ClusterMarkers_0),
-             "ClusterMarkers_0_avg_logFC": dfToNamedMatrix(ClusterMarkers_0_avg_logFC),
-             "ClusterMarkers_0_pval": dfToNamedMatrix(ClusterMarkers_0_pval)}
+row_attrs = {
+    "Gene": np.array(adata.raw.var.index),
+    "ClusterMarkers_0": dfToNamedMatrix(ClusterMarkers_0),
+    "ClusterMarkers_0_avg_logFC": dfToNamedMatrix(ClusterMarkers_0_avg_logFC),
+    "ClusterMarkers_0_pval": dfToNamedMatrix(ClusterMarkers_0_pval)
+}
 
 attrs = {"MetaData": json.dumps(metaJson)}
 
 attrs['MetaData'] = base64.b64encode(zlib.compress(json.dumps(metaJson).encode('ascii'))).decode('ascii')
 
-lp.create(filename=f"{FILE_PATH_OUT_BASENAME}.loom", layers=(adata.raw.X).T, row_attrs=row_attrs, col_attrs=col_attrs, file_attrs=attrs)
+lp.create(
+    filename=f"{FILE_PATH_OUT_BASENAME}.loom",
+    layers=(adata.raw.X).T,
+    row_attrs=row_attrs,
+    col_attrs=col_attrs,
+    file_attrs=attrs
+)
