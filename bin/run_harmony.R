eaa7d3143339638daf23e5db28b7962a892ff43e
dweemx <maxime.dewaegeneer@kuleuven.vib.be>
2020-02-12 10:21:21 +0100

    Create R script to run Harmony

---
+++ bin/run_harmony.R
@@ -0,0 +1,89 @@
+#!/usr/bin/env Rscript
+
+print("##################################################")
+print("# Harmony: Algorithm for single cell integration #")
+print("##################################################")
+
+# Loading dependencies scripts
+
+library("optparse")
+parser <- OptionParser(
+  prog = "run_harmony.R",
+  description = "Scalable integration of single cell RNAseq data for batch correction and meta analysis"
+)
+parser <- add_option(
+  parser,
+  c("-i", "--input-file"),
+  action = "store",
+  default = NULL,
+  help = "Input file [default]"
+)
+parser <- add_option(
+  parser,
+  c("-a", "--vars-use"),
+  action = "store",
+  default = NULL,
+  help = "If meta_data is dataframe, this defined which variable(s) to remove (character vector)."
+)
+parser <- add_option(
+  parser,
+  c("-p", "--do-pca"),
+  action = "store",
+  default = FALSE,
+  help = "Whether to perform PCA on input matrix."
+)
+parser <- add_option(
+  parser,
+  c("-o", "--output-prefix"),
+  action = "store",
+  default = "foo",
+  help="Prefix path to save output files. [default %default]"
+)
+
+args <- parse_args(parser)
+
+cat("Parameters: \n")
+print(args)
+
+if(is.null(args$`vars-use`)) {
+	stop("The parameter --vars-use has to be set.")
+}
+
+input_ext <- tools::file_ext(args$`input-file`)
+
+if(input_ext == "h5ad") {
+  seurat <- Seurat::ReadH5AD(file = args$`input-file`)
+  if(!("pca" %in% names(seurat@reductions)) || is.null(x = seurat@reductions$pca))
+    stop("Expects a PCA embeddings data matrix but it does not exist.")
+  data <- seurat@reductions$pca
+  metadata <- seurat@meta.data
+} else {
+  stop(paste0("Unrecognized input file format: ", input_ext, "."))
+}
+
+print(paste0("PCA embeddings matrix has ", dim(x = data)[1], " rows, ", dim(x = data)[2], " columns."))
+
+if(sum(args$`vars-use` %in% colnames(x = metadata)) != length(x = args$`vars-use`)) {
+	stop("Some argument value from the parameter(s) --vars-use are not found in the metadata.")
+}
+
+# Run Harmony
+harmony_embeddings <- harmony::HarmonyMatrix(data_mat = data@cell.embeddings
+                                             , meta_data = metadata
+                                             , vars_use = args$`vars-use`
+                                             , do_pca = args$`do-pca`
+                                             , verbose = FALSE
+)
+
+# Save the results
+
+## PCA corrected embeddings
+
+write.table(
+	x = harmony_embeddings,
+	file = paste0(args$`output-prefix`, ".tsv"),
+	quote = FALSE,
+	sep = "\t",
+	row.names = TRUE,
+	col.names = NA
+)
