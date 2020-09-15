7f60403ea1a4e2a586975d4c18da92a9d9e219dd
dweemx <maxime.dewaegeneer@kuleuven.vib.be>
2020-09-15 17:47:43 +0200

    Implement sample-based annotation (sample+) when additional annotation is required for indexing cells i.e.: sample ID/name is not enough (e.g.: metadata from data that was demultiplexed using demuxlet) Implement logic in sc_h5ad_annotate_by_sample_metadata.py Add test in main.test.nf New way in config to use this feature (see conf/sample_annotate.config): backward compatible change. Old way still works (utils/sample_annotate_old_v1.config)

--- src/utils/bin/sc_h5ad_annotate_by_sample_metadata.py
+++ src/utils/bin/sc_h5ad_annotate_by_sample_metadata.py
@@ -20,20 +20,61 @@ parser.add_argument(
 )
 
 parser.add_argument(
-    "-t", "--type",
+    "-s", "--sample-id",
     type=str,
     action="store",
-    dest="type",
-    default="sample",
-    help="Are the entries of the meta data sample-based or cell-based? Choose one of: sample or cell"
+    dest="sample_id",
+    help="The sample ID of the given h5ad file."
 )
 
 parser.add_argument(
-    "-m", "--meta-data-file-path",
+    "-m", "--method",
     type=str,
     action="store",
-    dest="meta_data_file_path",
+    dest="method",
+    default="sample",
+    choices=["sample", "sample+"],
+    help="Method used to do the sample-based annotation. Besides the sample ID, if another annotation column is required to annotate the cells, use 'sample+'."
+)
+
+parser.add_argument(
+    "-f", "--metadata-file-path",
+    type=str,
+    action="store",
+    dest="metadata_file_path",
     help="Path to the meta data. It expects a tabular separated file (.tsv) with header and a required 'id' column."
+)
+
+parser.add_argument(
+    "-t", "--sample-column-name",
+    type=str,
+    action="store",
+    dest="sample_column_name",
+    help="The name of the column of the given metadata containing the sample ID."
+)
+
+parser.add_argument(
+    '-i', '--adata-comp-index-column-name',
+    type=str,
+    action="append",
+    dest="adata_comp_index_column_names",
+    help="The names of complementary columns to use as index with the sample ID/name to annotate the cells. These column names should exist in the obs slot of the given h5ad input and have a 1-to-1 mapping with the -j/--metadata-comp-index-column-name."
+)
+
+parser.add_argument(
+    '-j', '--metadata-comp-index-column-name',
+    type=str,
+    action="append",
+    dest="metadata_comp_index_column_names",
+    help="The names of complementary columns to use as index with the sample ID/name to annotate the cells.  These column names should exist in the obs slot of the given h5ad input and have a 1-to-1 mapping with the -i/--adata-comp-index-column-name"
+)
+
+parser.add_argument(
+    '-a', '--annotation-column-name',
+    type=str,
+    action="append",
+    dest="annotation_column_names",
+    help="The names of columns from the given metadata to keep."
 )
 
 args = parser.parse_args()
@@ -41,7 +82,7 @@ args = parser.parse_args()
 # Define the arguments properly
 FILE_PATH_IN = args.input
 FILE_PATH_OUT_BASENAME = os.path.splitext(args.output.name)[0]
-SAMPLE_NAME = os.path.splitext(FILE_PATH_OUT_BASENAME)[0]
+SAMPLE_NAME = args.sample_id
 
 # I/O
 # Expects h5ad file
@@ -54,26 +95,49 @@ except IOError:
 # Annotate the data
 #
 
+if "sample_id" not in adata.obs.columns:
+    raise Exception("VSN ERROR: Missing sample_id column in the obs slot of the AnnData of the given h5ad.")
+
+if args.sample_column_name is None:
+    raise Exception("VSN ERROR: sampleColumnName is missing in the sample_annotate config.")
+
 metadata = pd.read_csv(
-    filepath_or_buffer=args.meta_data_file_path,
+    filepath_or_buffer=args.metadata_file_path,
     sep="\t"
 )
 
-if 'id' not in metadata.columns:
-    raise Exception("VSN ERROR: The meta data TSV file expects a header with a required 'id' column.")
+sample_info = metadata[metadata[args.sample_column_name] == SAMPLE_NAME]
 
-if args.type == "sample":
-    sample_info = metadata[metadata.id == SAMPLE_NAME]
+if len(sample_info) == 0:
+    raise Exception(f"VSN ERROR: The meta data TSV file does not contain sample ID '{SAMPLE_NAME}'.")
+elif args.method == "sample" and len(sample_info) > 1:
+    raise Exception(f"VSN ERROR: The meta data TSV file contains duplicate entries with the sample ID '{SAMPLE_NAME}'. Fix your metadata or use the 'sample+' method.")
 
-    if len(sample_info) == 0:
-        raise Exception(f"VSN ERROR: The meta data TSV file does not contain sample ID '{SAMPLE_NAME}'.")
-    elif len(sample_info) > 1:
-        raise Exception(f"VSN ERROR: The meta data TSV file contains more than one line with sample ID '{SAMPLE_NAME}'.")
-
+if args.method == "sample":
     for (column_name, column_data) in sample_info.iteritems():
         adata.obs[column_name] = column_data.values[0]
+elif args.method == "sample+":
+
+    if args.adata_comp_index_column_names is None or args.metadata_comp_index_column_names is None:
+        raise Exception("VSN ERROR: compIndexColumnNames param is missing in the sample_annotate config.")
+
+    new_obs = pd.merge(
+        adata.obs,
+        sample_info,
+        left_on=["sample_id"] + args.adata_comp_index_column_names,
+        right_on=[args.sample_column_name] + args.metadata_comp_index_column_names
+    )
+
+    if new_obs.isnull().values.any():
+        raise Exception("VSN ERROR: Merged adata.obs not complete, some NaN values detected.")
+
+    # Update the obs slot of the AnnData
+    adata.obs = new_obs
 else:
     raise Exception("VSN ERROR: This meta data type {} is not implemented".format(args.type))
+
+if args.annotation_column_names is not None and len(args.annotation_column_names) > 0:
+    adata.obs = adata.obs[args.annotation_column_names]
 
 # I/O
 adata.write_h5ad("{}.h5ad".format(FILE_PATH_OUT_BASENAME))
