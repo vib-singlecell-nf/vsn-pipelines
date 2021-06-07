b69c51a858ea9923eb2e106e3bc134171e7a3144
Gert Hulselmans <gert.hulselmans@kuleuven.be>
2021-06-07 14:44:46 +0200

    Cleanup bash code of mapping_summary.sh script.
    Cleanup bash code of mapping_summary.sh script:
      - Use double quotes around variable expansions.
      - Add Usage information if wrong number of arguments is given.
      - Cleanup comments.

--- src/bwamaptools/bin/mapping_summary.sh
+++ src/bwamaptools/bin/mapping_summary.sh
@@ -1,22 +1,27 @@
 #!/usr/bin/env bash
 
-sampleId="$1"
-bam="$2"
+sampleId="${1}";
+bam="${2}";
+
+if [ ${#@} -ne 2 ] ; then
+    printf 'Usage: mapping_summary.sh sampleId bam_file\n' >&2;
+    exit 1;
+fi
 
 
-# run samtools stat:
-samtools stat ${bam} > ${sampleId}.stat
+# Run samtools stat:
+samtools stat "${bam}" > "${sampleId}.stat"
 
-# uniquely mapped reads (BWA):
-UMR=$(samtools view -F 0x4 -F 0x100 -F 0x800 ${bam} | grep -v -e 'XA:Z:' -e 'SA:Z:' | wc -l)
+# Uniquely mapped reads (BWA):
+UMR=$(samtools view -F 0x4 -F 0x100 -F 0x800 "${bam}" | grep -v -e 'XA:Z:' -e 'SA:Z:' | wc -l);
 
-# Fraction of total read pairs mapped confidently to genome (>30 mapq)
-CMR=$(samtools view -c -F 0x4 -F 0x100 -F 0x800 -q 30 ${bam})
+# Fraction of total read pairs mapped confidently to genome (>30 mapq):
+CMR=$(samtools view -c -F 0x4 -F 0x100 -F 0x800 -q 30 "${bam}");
 
 
-# output file:
-printf "\t${sampleId}\n" > ${sampleId}.mapping_stats.tsv
-grep ^SN ${sampleId}.stat | cut -f 2,3 >> ${sampleId}.mapping_stats.tsv
-printf "Uniquely mapped reads:\t${UMR}\n" >> ${sampleId}.mapping_stats.tsv
-printf "Reads mapped with MAPQ>30:\t${CMR}\n" >> ${sampleId}.mapping_stats.tsv
+# Output file:
+printf "\t${sampleId}\n" > "${sampleId}.mapping_stats.tsv";
+grep '^SN' "${sampleId}.stat" | cut -f 2,3 >> "${sampleId}.mapping_stats.tsv";
+printf "Uniquely mapped reads:\t${UMR}\n" >> "${sampleId}.mapping_stats.tsv";
+printf "Reads mapped with MAPQ>30:\t${CMR}\n" >> "${sampleId}.mapping_stats.tsv";
 
