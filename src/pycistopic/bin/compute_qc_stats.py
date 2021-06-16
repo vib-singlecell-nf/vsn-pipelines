99fe0a62c64cc6023425dc2dd28b6dcb371fc0fb
Chris Flerin <ccflerin@gmail.com>
2021-06-16 15:20:30 +0200

    pycistopic compute_qc_stats update: - Load all input fragments/peak files at once with mulitple calls to --input_files, then parse this within the script - samples are parallelized using pycistopic/ray - remove metadata text output

--- src/pycistopic/bin/compute_qc_stats.py
+++ src/pycistopic/bin/compute_qc_stats.py
@@ -17,28 +17,8 @@ parser.add_argument(
     type=str,
     required=True,
     nargs='+',
+    action='append',
     help='Input files in the form of [SampleId, path_to_fragments, path_to_peaks]. Multiple inputs are possible.'
-)
-parser.add_argument(
-    "--sampleId",
-    type=str,
-    required=True,
-    nargs='+',
-    help='Sample ID.'
-)
-parser.add_argument(
-    "--fragments",
-    type=str,
-    required=True,
-    nargs='+',
-    help='Input fragments file.'
-)
-parser.add_argument(
-    "--regions",
-    type=str,
-    required=True,
-    nargs='+',
-    help='Path to regions file.'
 )
 parser.add_argument(
     "--n_frag",
@@ -51,11 +31,6 @@ parser.add_argument(
     "--biomart_annot_pkl",
     type=str,
     help='Biomart annotations, pickle format.'
-)
-parser.add_argument(
-    "--output_metadata",
-    type=str,
-    help='Output file, tsv format.'
 )
 parser.add_argument(
     "--output_metadata_pkl",
@@ -79,51 +54,44 @@ args = parser.parse_args()
 
 ################################################################################
 
+
+fragments_dict = { x[0].split(',')[0]: x[0].split(',')[1] for x in args.input_files }
+path_to_regions = { x[0].split(',')[0]: x[0].split(',')[2] for x in args.input_files }
+
 # Load biomart annotations:
 infile = open(args.biomart_annot_pkl, 'rb')
 annot = pickle.load(infile)
 infile.close()
 
 
-print(args.input_files)
-
-
-fragments_dict = { 
-    args.sampleId: args.fragments
-    }
-path_to_regions = {
-    args.sampleId: args.regions
-    }
 
 metadata_bc_dict, profile_data_dict = compute_qc_stats(
-                fragments_dict= fragments_dict,
-                tss_annotation = annot,
+                fragments_dict=fragments_dict,
+                tss_annotation=annot,
                 stats=['barcode_rank_plot', 'duplicate_rate', 'insert_size_distribution', 'profile_tss', 'frip'],
-                label_list = None,
-                path_to_regions = path_to_regions,
-                n_cpu = args.threads,
-                valid_bc = None,
-                n_frag = args.n_frag,
-                n_bc = None,
-                tss_flank_window = 1000,
-                tss_window = 50,
+                label_list=None,
+                path_to_regions=path_to_regions,
+                n_cpu=args.threads,
+                valid_bc=None,
+                n_frag=args.n_frag,
+                n_bc=None,
+                tss_flank_window=1000,
+                tss_window=50,
                 tss_minimum_signal_window = 100,
                 tss_rolling_window = 10,
+                min_norm=0.1,
                 remove_duplicates = True,
-                ### ray init args:
-                include_dashboard=False,
+                #_temp_dir=
                 )
 
 
-# load bap results to use for duplicate rate (if we are using bap output):
-f_bap_qc = os.path.join(os.path.dirname(args.fragments),args.sampleId+'.QCstats.csv')
-if os.path.isfile(f_bap_qc) and all(metadata_bc_dict[args.sampleId]['Dupl_rate_bap'] == 0):
-    bapqc = pd.read_csv(f_bap_qc, index_col=0)
-    metadata_bc_dict[args.sampleId]['Dupl_rate'] = bapqc['duplicateProportion']
+## load bap results to use for duplicate rate (if we are using bap output):
+#f_bap_qc = os.path.join(os.path.dirname(args.fragments),args.sampleId+'.QCstats.csv')
+#if os.path.isfile(f_bap_qc) and all(metadata_bc_dict[args.sampleId]['Dupl_rate_bap'] == 0):
+#    bapqc = pd.read_csv(f_bap_qc, index_col=0)
+#    metadata_bc_dict[args.sampleId]['Dupl_rate'] = bapqc['duplicateProportion']
 
 ### outputs:
-
-metadata_bc_dict[args.sampleId].to_csv(args.output_metadata, sep='\t', index_label='barcode')
 
 with open(args.output_metadata_pkl, 'wb') as f:
     pickle.dump(metadata_bc_dict, f)
